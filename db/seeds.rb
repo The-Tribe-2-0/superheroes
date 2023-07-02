c2e8ccac089830c6a6afc3814740a3e7c1d5c703
dMuguti <omiahderek56@gmail.com>
2023-07-02 22:55:51 +0300

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+Hero.destroy_all
+Power.destroy_all
+HeroPower.destroy_all
+
+heroes = [
+  { name: 'Kamala Khan', super_name: 'Ms. Marvel' },
+  { name: 'Doreen Green', super_name: 'Squirrel Girl' },
+  { name: 'Gwen Stacy', super_name: 'Spider-Gwen' }
+]
+
+powers = [
+  { name: 'Super Strength', description: 'Gives the wielder super-human strength' },
+  { name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed' }
+]
+
+heroes.each do |hero_attrs|
+  Hero.create(hero_attrs)
+end
+
+powers.each do |power_attrs|
+  Power.create(power_attrs)
+end
+
+heroes = Hero.all
+powers = Power.all
+
+heroes.each do |hero|
+  powers.each do |power|
+    HeroPower.create(strength: ['Strong', 'Average', 'Weak'].sample, hero: hero, power: power)
+  end
+end
